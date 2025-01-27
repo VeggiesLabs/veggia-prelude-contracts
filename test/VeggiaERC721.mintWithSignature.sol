df7478239604669c99c655232ae0837d758d7623
Léo FASANO <fasanol.info@gmail.com>
2025-01-27 16:38:38 +0100

    feat: refactor and enhance mintWithSignature tests for VeggiaERC721

--- test/VeggiaERC721.mintWithSignature.sol
+++ test/VeggiaERC721.mintWithSignature.sol
@@ -8,29 +8,8 @@ import {SERVER_SIGNER} from "./utils/constants.sol";
 contract VeggiaERC721MintWithSignatureTest is Test {
     VeggiaERC721 public veggia;
 
-    function test_raw_mintWithSignature() public {
-        address serverSigner = 0x2f9e4Ffc85257247e9061A3EE9d3b6b23eF560E9;
-        veggia = new VeggiaERC721(address(msg.sender), "http://localhost:4000/");
-        veggia.initialize(address(this), address(this), serverSigner, "http://localhost:4000/");
-        assertEq(veggia.capsSigner(), serverSigner);
-
-        // message
-        bytes memory message =
-            hex"000000000000000000000000e5af443d0f924e31a19b2286f8df3c60de97963dfa5920d307f7bf370eda6b4ce25abc6b757e5e47558c05b611e05832eafb0cee0000000000000000000000000000000000000000000000000000000000000000";
-        (address to, uint256 index) = abi.decode(message, (address, uint256));
-        assertEq(to, address(0xE5aF443D0F924E31a19b2286F8DF3c60dE97963D));
-        assertEq(index, uint256(0xfa5920d307f7bf370eda6b4ce25abc6b757e5e47558c05b611e05832eafb0cee));
-
-        // signature
-        bytes memory signature =
-            hex"79477f1c5e288bbe48e8571544ad47bb903d35feb4ec25230d7ddd9b2ac38b2d4864e90af6a3a59bc95ad6d42589259f411b80f4f5a155e049e6b5dd9f630fc21c";
-
-        // mint
-        vm.prank(to);
-        veggia.mintWithSignature(signature, message);
-    }
-
     function test_mintWithSignature(string memory random, uint256 index, bool isPremium, address user) public {
+        vm.assume(user != address(0));
         (address serverSigner, uint256 signer) = makeAddrAndKey(random);
 
         veggia = new VeggiaERC721(address(msg.sender), "http://localhost:4000/");
@@ -55,6 +34,7 @@ contract VeggiaERC721MintWithSignatureTest is Test {
     function test_MintWithSignatureWrongSigner(string memory random, uint256 index, bool isPremium, address user)
         public
     {
+        vm.assume(user != address(0));
         (, uint256 signer) = makeAddrAndKey(random);
 
         veggia = new VeggiaERC721(address(msg.sender), "http://localhost:4000/");
@@ -79,6 +59,8 @@ contract VeggiaERC721MintWithSignatureTest is Test {
     function test_MintWithSignatureReusedSignature(string memory random, uint256 index, bool isPremium, address user)
         public
     {
+        vm.assume(user != address(0));
+
         (address serverSigner, uint256 signer) = makeAddrAndKey(random);
 
         veggia = new VeggiaERC721(address(msg.sender), "http://localhost:4000/");
@@ -108,6 +90,8 @@ contract VeggiaERC721MintWithSignatureTest is Test {
     function test_MintWithSignatureInvalidSender(string memory random, uint256 index, bool isPremium, address user)
         public
     {
+        vm.assume(user != address(0));
+
         (address serverSigner, uint256 signer) = makeAddrAndKey(random);
 
         veggia = new VeggiaERC721(address(msg.sender), "http://localhost:4000/");
@@ -132,5 +116,27 @@ contract VeggiaERC721MintWithSignatureTest is Test {
         assertEq(veggia.balanceOf(user), 0);
         assertEq(veggia.balanceOf(invalidSender), 0);
         assertEq(veggia.tokenId(), 0);
+    }
+
+    function test_raw_mintWithSignature() public {
+        address serverSigner = 0x2f9e4Ffc85257247e9061A3EE9d3b6b23eF560E9;
+        veggia = new VeggiaERC721(address(msg.sender), "http://localhost:4000/");
+        veggia.initialize(address(this), address(this), serverSigner, "http://localhost:4000/");
+        assertEq(veggia.capsSigner(), serverSigner);
+
+        // message
+        bytes memory message =
+            hex"000000000000000000000000e5af443d0f924e31a19b2286f8df3c60de97963dfa5920d307f7bf370eda6b4ce25abc6b757e5e47558c05b611e05832eafb0cee0000000000000000000000000000000000000000000000000000000000000000";
+        (address to, uint256 index) = abi.decode(message, (address, uint256));
+        assertEq(to, address(0xE5aF443D0F924E31a19b2286F8DF3c60dE97963D));
+        assertEq(index, uint256(0xfa5920d307f7bf370eda6b4ce25abc6b757e5e47558c05b611e05832eafb0cee));
+
+        // signature
+        bytes memory signature =
+            hex"79477f1c5e288bbe48e8571544ad47bb903d35feb4ec25230d7ddd9b2ac38b2d4864e90af6a3a59bc95ad6d42589259f411b80f4f5a155e049e6b5dd9f630fc21c";
+
+        // mint
+        vm.prank(to);
+        veggia.mintWithSignature(signature, message);
     }
 }
