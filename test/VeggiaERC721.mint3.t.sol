3a3144d011fd3441e6918b4977562e0942c29061
Léo FASANO <fasanol.info@gmail.com>
2025-01-28 15:23:42 +0100

    test: add unit tests for mint3

--- test/VeggiaERC721.mint3.t.sol
+++ test/VeggiaERC721.mint3.t.sol
@@ -11,25 +11,59 @@ contract VeggiaERC721Mint3Test is Test {
     function setUp() public {
         veggia = new VeggiaERC721(address(msg.sender), "http://localhost:4000/");
         address serverSigner = vm.addr(uint256(SERVER_SIGNER));
-        veggia.initialize(address(this), address(this), serverSigner, "http://localhost:4000/");
+        veggia.initialize(address(this), address(0x1234), serverSigner, "http://localhost:4000/");
     }
 
-    // missing mint3
-    // missing capsBalanceOf
-    // missing all onlyOwner functions
-    // missing _open3CapsForSender
-
-    function test_mint() public {
-        // 1. buy a caps
+    function test_mint3() public {
         assertEq(veggia.capsBalanceOf(address(this)), 0);
         assertEq(veggia.paidCapsBalanceOf(address(this)), 0);
 
         uint256 threeCapsPrice = veggia.capsPriceByQuantity(3);
-        veggia.buyCaps{value: threeCapsPrice}(false, 1);
+        veggia.buyCaps{value: threeCapsPrice}(false, 3);
 
-        assertEq(veggia.capsBalanceOf(address(this)), 1);
-        assertEq(veggia.paidCapsBalanceOf(address(this)), 1);
+        assertEq(veggia.capsBalanceOf(address(this)), 3);
+        assertEq(veggia.paidCapsBalanceOf(address(this)), 3);
 
-        // 2. Open the caps (mint 3 tokens)
+        veggia.mint3(false);
+
+        assertEq(veggia.capsBalanceOf(address(this)), 0);
+        assertEq(veggia.paidCapsBalanceOf(address(this)), 0);
+
+        assertEq(veggia.balanceOf(address(this)), 3);
+    }
+
+    function test_mint3Premium() public {
+        assertEq(veggia.capsBalanceOf(address(this)), 0);
+        assertEq(veggia.paidPremiumCapsBalanceOf(address(this)), 0);
+
+        uint256 threePremiumCapsPrice = veggia.premiumCapsPriceByQuantity(3);
+        veggia.buyCaps{value: threePremiumCapsPrice}(true, 3);
+
+        assertEq(veggia.capsBalanceOf(address(this)), 3);
+        assertEq(veggia.paidPremiumCapsBalanceOf(address(this)), 3);
+
+        veggia.mint3(true);
+
+        assertEq(veggia.capsBalanceOf(address(this)), 0);
+        assertEq(veggia.paidPremiumCapsBalanceOf(address(this)), 0);
+
+        assertEq(veggia.balanceOf(address(this)), 3);
+    }
+
+    function test_mint3WhenBalanceIsUero() public {
+        // 1. buy a caps
+        assertEq(veggia.capsBalanceOf(address(this)), 0);
+        assertEq(veggia.paidCapsBalanceOf(address(this)), 0);
+        assertEq(veggia.paidPremiumCapsBalanceOf(address(this)), 0);
+
+        vm.expectRevert(abi.encodeWithSelector(VeggiaERC721.INSUFFICIENT_CAPS_BALANCE.selector));
+        veggia.mint3(true);
+        vm.expectRevert(abi.encodeWithSelector(VeggiaERC721.INSUFFICIENT_CAPS_BALANCE.selector));
+        veggia.mint3(false);
+
+        assertEq(veggia.capsBalanceOf(address(this)), 0);
+        assertEq(veggia.paidCapsBalanceOf(address(this)), 0);
+
+        assertEq(veggia.paidPremiumCapsBalanceOf(address(this)), 0);
     }
 }
